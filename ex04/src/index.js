9c3633bd49d4ef044f6c3836c6edccea0eacfbda
mirza-z <zaimovic330@gmail.com>
2022-04-11 18:55:56 +0200

    Adding some files go gitHub

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+const milili = [10, 25, 4];
+function myArr(mili) {
+    "use strict";
+    mili[0] = 4;
+    mili[1] = 10;
+    mili[2] = 25;
+    // Using mili = [4, 10, 25] would be invalid
+    return mili;
+}
+console.log(myArr(milili));
+// Only change code above this line
+module.exports = myArr;
